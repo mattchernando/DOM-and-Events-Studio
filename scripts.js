a08682270914215e42c8fc79f7841d3fc57943a6
Matt Hernando <mhernando@unknownc2a5119ecc33.attlocal.net>
2022-02-28 20:13:18 -0600

    turning in assignment

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener('load',function() {
+let myBtn = document.getElementById('takeoff');
+myBtn.addEventListener('click',function() {
+    if (window.confirm("Confirm that the shuttle is ready for takeoff")) {
+    let myFlightStatus = document.getElementById('flightStatus');
+    myFlightStatus.innerText ="Shuttle In Flight";
+    
+    let shuttleBg = document.getElementById('shuttleBackground');
+    shuttleBg.setAttribute('style','background-color: blue');
+
+    let shuttleHeight = document.getElementById('spaceShuttleHeight');
+    shuttleHeight.innerText = "10,000";
+    }
+});
+
+});
